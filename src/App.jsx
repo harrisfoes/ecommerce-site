9c65b692ec10596bfa0f4d7361e1e6a64e20f7c5
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2023-11-27 19:33:57 +0900

    first mobile pass with all ui until category

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import Homepage from "./components/pages/Homepage";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-blue-900">
-        Tailwind is working you fool
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex flex-col items-center">
+        <Homepage />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#000000",
+      blue: "#377dff",
+      green: "#38cb89",
+      orange: "#FFAB00",
+      red: "#FF5630",
+      "neutral-100": "#FEFEFE",
+      "neutral-200": "#F3F5F7",
+      "neutral-300": "#E8ECEF",
+      "neutral-400": "#6C7275",
+      "neutral-500": "#343839",
+      "neutral-600": "#232627",
+      "neutral-700": "#141718",
+    },
+    fontSize: {
+      xs: "12px",
+      sm: "14px",
+      md: "16px",
+      lg: "18px",
+      xl: "20px",
+      "2xl": "22px",
+      "3xl": "26px",
+      "4.5xl": "40px",
+    },
+    fontFamily: {
+      poppins: ["Poppins"],
+      inter: ["Inter"],
+    },
     extend: {},
   },
+
   plugins: [],
 };
